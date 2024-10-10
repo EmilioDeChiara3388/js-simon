2f9762893a61d5b3807d380c140370cdd1dffd64
EmilioDeChiara3388 <emilio.dechiara3388@gmail.com>
2024-10-10 15:37:01 +0200

    add: timer on display and form

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,14 +10,30 @@ NOTA: non è importante l'ordine con cui l'utente inserisce i numeri, basta che 
 //Preparazione:
 //Visualizzare in pagina 5 numeri casuali (Math random: 1-100);
 const pcNumbersEl = document.querySelector(".pcNumbers");
+const userNumbersEl = document.querySelector(".userNumbers");
+const timerEl = document.querySelector(".timer");
 
 for (let i = 0; i < 5; i++){
     let randomNumbers = Math.floor(Math.random() * 100) + 1;
     console.log(randomNumbers);
     pcNumbersEl.innerHTML += `<p>${randomNumbers}</p>`;
-}
-
+};
 
 //Far partire timer di 30 secondi;
 //Alla fine del timer, far sparire i numeri, e mettere input per inserire 5 numeri dall'user;
+let seconds = 3;
+let clock = setInterval(countDown, 3000);
+
+function countDown(){
+    if (seconds == 0){
+        userNumbersEl.classList.remove("guess");
+        timerEl.classList.add("guess");
+        clearInterval(clock);
+    } else {
+        timerEl.innerHTML = seconds;
+        seconds --;
+    }
+}
+
+
 //Confrontare numeri dell'user con quelli random iniziare, e dare valori di risposta.
