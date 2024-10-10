382dab10a1fd4a9b5ea797c094d36a8ddf51f9d5
EmilioDeChiara3388 <emilio.dechiara3388@gmail.com>
2024-10-10 16:53:21 +0200

    add: boostrap form

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-console.log("Funzioni?");
+//console.log("Funzioni?");
 /* 
 Visualizzare in pagina 5 numeri casuali.
 Da lì parte un timer di 30 secondi.
@@ -12,28 +12,35 @@ NOTA: non è importante l'ordine con cui l'utente inserisce i numeri, basta che 
 const pcNumbersEl = document.querySelector(".pcNumbers");
 const userNumbersEl = document.querySelector(".userNumbers");
 const timerEl = document.querySelector(".timer");
+const buttonEl = document.getElementById("button");
 
+let pcRandomNumbers = [];
 for (let i = 0; i < 5; i++){
     let randomNumbers = Math.floor(Math.random() * 100) + 1;
+    pcRandomNumbers.push(randomNumbers);
     console.log(randomNumbers);
     pcNumbersEl.innerHTML += `<p>${randomNumbers}</p>`;
 };
+console.log(pcRandomNumbers);
 
 //Far partire timer di 30 secondi;
 //Alla fine del timer, far sparire i numeri, e mettere input per inserire 5 numeri dall'user;
 let seconds = 3;
-let clock = setInterval(countDown, 3000);
+let clock = setInterval(countDown, 1000);
 
 function countDown(){
     if (seconds == 0){
+        pcNumbersEl.classList.add("guess");
         userNumbersEl.classList.remove("guess");
         timerEl.classList.add("guess");
         clearInterval(clock);
     } else {
-        timerEl.innerHTML = seconds;
+        timerEl.innerHTML = ("Ti restano " + seconds + " secondi");
         seconds --;
     }
 }
 
-
 //Confrontare numeri dell'user con quelli random iniziare, e dare valori di risposta.
+/* buttonEl.addEventListener("submit", function(e){
+    e.preventDefault();
+}); */
