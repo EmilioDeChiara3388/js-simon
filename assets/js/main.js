7eda29d18e5095926722d1d63a60a9f1e8c95f0c
EmilioDeChiara3388 <emilio.dechiara3388@gmail.com>
2024-10-10 14:22:18 +0200

    add: preparation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,14 @@
 console.log("Funzioni?");
+/* 
+Visualizzare in pagina 5 numeri casuali.
+Da lì parte un timer di 30 secondi.
+Dopo 30 secondi i numeri scompaiono e appaiono invece 5 input in cui l'utente deve inserire i numeri che ha visto precedentemente, nell'ordine che preferisce.
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+NOTA: non è importante l'ordine con cui l'utente inserisce i numeri, basta che ne indovini il più possibile.
+*/
+
+//Preparazione:
+//Visualizzare in pagina 5 numeri casuali (Math random: 1-100);
+//Far partire timer di 30 secondi;
+//Alla fine del timer, far sparire i numeri, e mettere input per inserire 5 numeri dall'user;
+//Confrontare numeri dell'user con quelli random iniziare, e dare valori di risposta.
